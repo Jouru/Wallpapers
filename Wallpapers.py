e796154fb05ff058f01881d30d63ffcd809397a1
Jouru <104511948+Jouru@users.noreply.github.com>
2023-01-13 00:42:37 +0100

    set the config file and CLI arguments

--- Wallpapers.py
+++ Wallpapers.py
@@ -1,16 +1,71 @@
 import os
-import time
+import argparse
+from time import sleep
 from random import randint
 
-def main():
-    pth = "~/Wallpapers" ## Change for the path of your wallpapers directory
-    pics = os.listdir(path)
 
-    while True:
-        i = randint(0,len(pics))
-        picture = f"{pth}/{pics[i]}"
-        os.system(f"feh --bg-fill {picture}")
-        time.sleep(900) ## Time is given in seconds
+def main():
+    parser = argparse.ArgumentParser(description="Wallpapers")
+    parser.add_argument("--set", help="Set the default values", action="store_true")
+    parser.add_argument("-time", help="Define time in minutes", type=int)
+    parser.add_argument("-path", help="Define path")
+    args = parser.parse_args()
+    
+    ## Create the config file
+    if not os.path.isfile("wallpapers.conf"):
+        with open("wallpapers.conf", "w") as file:
+            time = "3600"
+            path = "/home/abner/.config/qtile/Wallpapers/"
+            file.write(f"Time= {time}\n")
+            file.write(f"PATH= {path}")
+            
+    if args.set:
+        ## Define a new time
+        if args.time:
+            new_time = args.time*60
+            with open("wallpapers.conf", "r") as file:
+                lines = file.readlines()
+            for l in range(len(lines)):
+                if lines[l].startswith("Time="):
+                    lines[l] = f"Time= {new_time}"
+            
+            with open("wallpapers.conf", "w") as file:
+                for l in range(len(lines)):
+                    file.write(f"{lines[l].strip()}\n")
+        
+        ## Define a new path
+        if args.path:
+            with open("wallpapers.conf", "r") as file:
+                lines = file.readlines()
+            for l in range(len(lines)):
+                if lines[l].startswith("PATH="):
+                    lines[l] = f"PATH= {args.path}"
+            
+            with open("wallpapers.conf", "w") as file:
+                for l in range(len(lines)):
+                    file.write(f"{lines[l].strip()}\n")
+    
+    else:    
+        with open("wallpapers.conf", "r") as file:
+            new_lines = file.readlines()         
+
+        ## Get time and path to wallpapers directory
+        pth = new_lines[1].lstrip("PATH= ").strip()
+        time = int(new_lines[0].strip("Time= "))
+     
+        while True:
+            ## Get the pictures names
+            pics = os.listdir(pth)
+            
+            ## Set the wallpaper
+            i = randint(0, (len(pics) - 1))
+            picture = f"{pth}/{pics[i]}"
+            os.system(f"feh --bg-fill {picture}")
+            
+            ## Time until the new wallpaper
+            sleep(time)            
+    
+    
 
 if __name__=="__main__":
     main()
