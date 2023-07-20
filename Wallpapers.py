44c67a380774bb9a66b271297947785bd80a1d30
Jouru <104511948+Jouru@users.noreply.github.com>
2023-07-20 14:40:42 +0200

    Add files via upload

--- Wallpapers.py
+++ Wallpapers.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 from time import sleep
 from random import randint
@@ -9,6 +10,9 @@ def main():
     parser.add_argument("--set", help="Set default values", action="store_true")
     parser.add_argument("-time", help="Define time in minutes", type=int)
     parser.add_argument("-path", help="Define path")
+    parser.add_argument(
+        "--theme", help="Define Wallpapers theme, case sensitive", action="store_true"
+    )
     args = parser.parse_args()
 
     # Create the config file
@@ -16,9 +20,11 @@ def main():
     if not os.path.isfile(config_file):
         with open(config_file, "w") as file:
             time = "60"
-            path = "~/Wallpapers"
+            path = "/home/abner/Wallpapers"
+            theme = "pictures"
             file.write(f"Time= {time}\n")
-            file.write(f"PATH= {path}")
+            file.write(f"PATH= {path}\n")
+            file.write(f"THEME= {theme}")
 
     if args.set:
         # Define a new time
@@ -46,12 +52,33 @@ def main():
                 for line in range(len(lines)):
                     file.write(f"{lines[line].strip()}\n")
 
+    # Define the wallpapers theme
+    elif args.theme:
+        with open(config_file, "r") as file:
+            lines = file.readlines()
+        themes = os.listdir(lines[1].lstrip("PATH= ").strip())
+        for theme in themes:
+            print(theme, end=" ")
+        theme = input("\nWhich theme do you want apply ")
+
+        if theme not in themes:
+            sys.exit(f"{theme} doesn't exist in your themes")
+
+        for line in range(len(lines)):
+            if lines[line].startswith("THEME="):
+                lines[line] = f"THEME= {theme}"
+
+        with open(config_file, "w") as file:
+            for line in lines:
+                file.write(f"{line.strip()}\n")
+
     else:
         with open(config_file, "r") as file:
             new_lines = file.readlines()
 
         # Get time and path to wallpapers directory
-        pth = new_lines[1].lstrip("PATH= ").strip()
+        _, sub_pth = new_lines[2].split(" ")
+        pth = f"{new_lines[1].lstrip('PATH= ').strip()}/{sub_pth.strip()}"
         time = int(new_lines[0].strip("Time= ")) * 60
 
         while True:
